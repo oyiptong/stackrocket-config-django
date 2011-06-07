79ea9ca35aca5b03c299f04839c5a196cf6ff761
oyiptong <oyiptong@zompf.com>
2011-06-07 00:09:15 -0400

    ubuntu ppas

--- chef/cookbooks/main/recipes/apt.rb
+++ chef/cookbooks/main/recipes/apt.rb
@@ -4,11 +4,46 @@ directory('/etc/apt') do
 	owner 'root'
 	recursive true
 end
-directory('/etc/apt/apt.conf.d') do
+directory('/etc/apt/sources.list.d') do
 	group 'root'
 	mode '0755'
 	owner 'root'
 	recursive true
+end
+cookbook_file('/etc/apt/sources.list.d/nginx-stable-lucid.list') do
+	backup false
+	group 'root'
+	mode '0600'
+	owner 'root'
+	source 'etc/apt/sources.list.d/nginx-stable-lucid.list'
+end
+cookbook_file('/etc/apt/sources.list.d/nginx-stable-lucid.list.save') do
+	backup false
+	group 'root'
+	mode '0600'
+	owner 'root'
+	source 'etc/apt/sources.list.d/nginx-stable-lucid.list.save'
+end
+cookbook_file('/etc/apt/sources.list.d/pitti-postgresql-lucid.list') do
+	backup false
+	group 'root'
+	mode '0600'
+	owner 'root'
+	source 'etc/apt/sources.list.d/pitti-postgresql-lucid.list'
+end
+cookbook_file('/etc/apt/sources.list.d/pitti-postgresql-lucid.list.save') do
+	backup false
+	group 'root'
+	mode '0600'
+	owner 'root'
+	source 'etc/apt/sources.list.d/pitti-postgresql-lucid.list.save'
+end
+cookbook_file('/etc/apt/sources.list.d/uwsgi-release-lucid.list') do
+	backup false
+	group 'root'
+	mode '0600'
+	owner 'root'
+	source 'etc/apt/sources.list.d/uwsgi-release-lucid.list'
 end
 cookbook_file('/etc/apt/secring.gpg') do
 	backup false
