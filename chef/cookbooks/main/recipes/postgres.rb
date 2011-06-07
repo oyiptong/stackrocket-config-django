148b81a2e30293e26eafa55d3f52a9f7bd1f090c
oyiptong <oyiptong@zompf.com>
2011-06-07 00:17:55 -0400

    fix postgres

--- chef/cookbooks/main/recipes/postgres.rb
+++ chef/cookbooks/main/recipes/postgres.rb
@@ -3,45 +3,19 @@ postgres_version = "9.0"
 package('postgresql')
 package('libpq-dev')
 
-directory('/etc/postgresql-common') do
-	group 'root'
-	mode '0755'
-	owner 'root'
-	recursive true
-end
-cookbook_file('/etc/postgresql-common/root.crt') do
-	backup false
-	group 'root'
-	mode '0644'
-	owner 'root'
-	source 'etc/postgresql-common/root.crt'
-end
-directory('/etc/postgresql/9.0/main') do
-	group 'root'
-	mode '0755'
-	owner 'root'
-	recursive true
-end
-cookbook_file('/etc/postgresql/9.0/main/environment') do
-	backup false
-	group 'postgres'
-	mode '0644'
-	owner 'postgres'
-	source 'etc/postgresql/9.0/main/environment'
-end
 cookbook_file('/etc/postgresql/9.0/main/pg_hba.conf') do
 	backup false
 	group 'postgres'
 	mode '0640'
 	owner 'postgres'
-	source 'etc/postgresql/9.0/main/pg_hba.conf'
+	source 'postgres/pg_hba.conf'
 end
 cookbook_file('/etc/postgresql/9.0/main/postgresql.conf') do
 	backup false
 	group 'postgres'
 	mode '0644'
 	owner 'postgres'
-	source 'etc/postgresql/9.0/main/postgresql.conf'
+	source 'postgres/postgresql.conf'
 end
 
 unless File.exists? "/tmp/db_init.sql"
